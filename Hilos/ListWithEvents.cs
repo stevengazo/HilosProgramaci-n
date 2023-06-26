031cebe234c9f6476e9d3b2d26f647e3a99f2bb1
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-06-26 09:11:19 -0600

    Pruebas

---
+++ Hilos/ListWithEvents.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Hilos
+{
+    public class ListWithEvents<T> : List<T>
+    {
+        private List<T> internalList = new List<T>();
+
+        public event EventHandler<ItemAddedEventArgs<T>> ItemAdded;
+
+        public void Add(T item)
+        {
+            internalList.Add(item);
+            OnItemAdded(item);
+        }
+
+        protected virtual void OnItemAdded(T item)
+        {
+            ItemAdded?.Invoke(this, new ItemAddedEventArgs<T>(item));
+        }
+    }
+
+    public class ItemAddedEventArgs<T> : EventArgs
+    {
+        public T Item { get; }
+        public ItemAddedEventArgs(T item)
+        {
+            Item = item;
+        }
+    }
+  
+
+}
