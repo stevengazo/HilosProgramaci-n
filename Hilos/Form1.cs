01e3d2e82a94e62e71a9a1695a213e168cea6edc
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-06-29 15:24:47 -0600

    en un solo hilo

--- Hilos/Form1.cs
+++ Hilos/Form1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 
 namespace Hilos
@@ -9,83 +10,112 @@ namespace Hilos
             InitializeComponent();
         }
 
+        #region Propiedades
+        /// <summary>
+        /// Arreglo contenedor de los items que se van a usar
+        /// </summary>
         short[] Numeros = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        /// <summary>
+        /// Lista para Guardar los resultados 
+        /// </summary>
         public List<double> ListaPares = new List<double>();
-        public List<double> ListaImpares = new List<double>();
+        /// <summary>
+        /// Lista para guardar los resultados de los numeros impares
+        /// </summary>
+        public List<decimal> ListaImpares = new List<decimal>();
+        /// <summary>
+        /// Cuenta la cantidad de hilos usados
+        /// </summary>
         int CantidadHilos = 0;
+        private Thread HiloImpar;
+        private Thread HiloPar;
+        #endregion
 
+        #region Metodos
 
-        void Potencia(int numero)
+        /// <summary>
+        /// Calcula la potencia y pone a dormir el hilo 1000 miliseconds
+        /// </summary>
+        /// <param name="numero">Número a calcular</param>
+        double Potencia(int numero)
         {
-            double Resultado = Math.Pow(numero, 2);
-            ListaPares.Add(Resultado);
-            CantidadHilos = Process.GetCurrentProcess().Threads.Count;
-            Thread.Sleep(1000);
+            double s = Math.Pow(numero, 2);
+            return s;
+
         }
-
-
-
-
+        /// <summary>
+        /// Ejecución de logica basica cuando se ejecuta el boton Ejecutar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnEjecutar_Click(object sender, EventArgs e)
         {
-            Thread HiloPar = new Thread(() => { });
-            dataGridViewNumeros.Columns.Add("ColumnNumeros", "Numeros");
-
-            for (int i = 0; i < Numeros.Length; i++)
+            try
             {
-                int NumeroAEvaluar = Numeros[i];
-                dataGridViewNumeros.Rows.Add(NumeroAEvaluar.ToString());
-                txtNumero.Text = NumeroAEvaluar.ToString();
-                if (NumeroAEvaluar % 2 == 0)
+                dataGridViewNumeros.Columns.Add("ColumnNumeros", "Numeros");
+                for (int i = 0; i < Numeros.Length; i++)
                 {
-                    txtTipo.Text = "Par";
-                    txtNumeroHilo.Text = CantidadHilos.ToString();
-                    // Calcular Potencia
-                    HiloPar = new Thread(new ThreadStart(() =>
+                    int NumeroAEvaluar = Numeros[i];
+                    dataGridViewNumeros.Rows.Add(NumeroAEvaluar.ToString());
+                    txtNumero.Text = NumeroAEvaluar.ToString();
+                    if (NumeroAEvaluar % 2 == 0)
                     {
-                        Potencia(i); 
-                    }));
-                    HiloPar.Start();
-                }
-                else
-                {
-                    txtTipo.Text = "Impar";
-                    // Calcular Factorial
-                    Thread hilo = new Thread(new ThreadStart(() =>
+                        txtTipo.Text = "Par";
+                        // Calcular Potencia
+                        ListaPares.Add(Potencia(NumeroAEvaluar));
+                    }
+                    else
                     {
-                        double Numero = NumeroAEvaluar!;
-                        ListaImpares.Add(NumeroAEvaluar);
-                        Thread.Sleep(100);
-                    }));
-                    hilo.Start();
-                   // hilo.Join();
-
+                        txtTipo.Text = "Impar";
+                        // Calcular Factorial
+                        ListaImpares.Add(Factorial(NumeroAEvaluar));
+                    }
                 }
             }
-            HiloPar.Join();
-            CargaGridViews();
+            catch (InvalidOperationException f)
+            {
+                MessageBox.Show(f.InnerException.ToString(), f.Message, MessageBoxButtons.OK);
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show(f.InnerException.ToString(), f.Message, MessageBoxButtons.OK);
+            }
+            finally
+            {
+                CargaGridViewsPar();
+                CargaGridViewsImpar();
+            }
+
         }
 
-
-        private void CargaGridViews()
+        /// <summary>
+        /// Carga la información de las listas en los dos GridViews para par e impar
+        /// </summary>
+        private void CargaGridViewsImpar()
         {
-
-            
-
+            dataGridViewImpares.Columns.Add("colPares", "Impares");
+            foreach (var item in ListaImpares )
+            {
+                dataGridViewImpares.Rows.Add(item.ToString());
+            }
+        }
+        /// <summary>
+        /// Carga la información de las listas en los dos GridViews para par e impar
+        /// </summary>
+        private void CargaGridViewsPar()
+        {
             dataGridViewPares.Columns.Add("colPares", "Pares");
-            dataGridViewImpares.Columns.Add("colImpares", "Impares");
-
 
             foreach (var item in ListaPares)
             {
                 dataGridViewPares.Rows.Add(item);
             }
-            foreach (var item in ListaPares)
-            {
-                dataGridViewImpares.Rows.Add(item);
-            }
         }
-
+        /// <summary>
+        /// Limpia la pantalla
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnLimpiar_Click(object sender, EventArgs e)
         {
             ListaImpares.Clear();
@@ -96,10 +126,30 @@ namespace Hilos
             dataGridViewPares.Columns.Clear();
             dataGridViewNumeros.Columns.Clear();
         }
-
+        /// <summary>
+        /// Cierra el programa
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+        /// <summary>
+        /// Calcula el numero factorial
+        /// </summary>
+        /// <param name="num">Numero a calcular</param>
+        /// <returns>retorno del dato</returns>
+        public decimal Factorial(int num)
+        {
+            decimal d = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                d = d * (i);
+            }
+            return d;
+        }
+        #endregion
     }
+
 }
