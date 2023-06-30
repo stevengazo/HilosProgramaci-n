3bff4e7ff54590e481390cf39f0a32a9d913989d
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2023-06-29 19:28:03 -0600

    Cambio en tamaño de lista

--- Hilos/Form1.cs
+++ Hilos/Form1.cs
@@ -8,10 +8,12 @@ namespace Hilos
         }
 
         #region Propiedades
+
+
         /// <summary>
         /// Arreglo contenedor de los items que se van a usar
         /// </summary>
-        short[] Numeros = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        short[] Numeros = new short[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         /// <summary>
         /// Lista para Guardar los resultados 
         /// </summary>
@@ -61,6 +63,8 @@ namespace Hilos
                         // Calcular Potencia
 
                         // Declaraci[on de hilo
+
+
                         Thread threadPar = new Thread(new ThreadStart(() =>
                         {
                             lock (this)
